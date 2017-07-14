6519d1e70ffc88c363896f9af3272961415d3efb
Mick Sexton <mick@>
2017-07-14 10:49:54 -0700

    add all initial tests for Stylist class

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -22,6 +22,64 @@ public class Stylist {
     }
   }
 
+  public static List<Stylist> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists;";
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  public static Stylist find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists WHERE id=:id;";
+      Stylist stylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Stylist.class);
+    return stylist;
+    }
+  }
+
+  public void updateWage(int wage) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE stylists SET wage = :wage WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("wage", wage)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void updateName(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE stylists SET name = :name WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM stylists WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherStylist) {
+    if(!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getId() == newStylist.getId() &&
+             this.getName().equals(newStylist.getName()) &&
+             this.getWage() == newStylist.getWage();
+    }
+  }
+
   public String getName() {
     return name;
   }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -18,10 +18,54 @@ public class StylistTest {
     assertEquals("Brenda", testStylist1.getName());
   }
   @Test
+  public void getWage_retrievesStylistWage_10() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    assertEquals(10, testStylist1.getWage());
+  }
+  @Test
   public void getId_instantiatesWithAnId_true() {
     Stylist testStylist1 = new Stylist("Brenda", 10);
     testStylist1.save();
     assertTrue(testStylist1.getId() > 1);
+  }
+  @Test
+  public void all_retrievesAllInstancesOfStylist_true() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    testStylist1.save();
+    Stylist testStylist2 = new Stylist("Gladis", 8);
+    testStylist2.save();
+    assertEquals(true, Stylist.all().get(0).equals(testStylist1));
+    assertEquals(true, Stylist.all().get(1).equals(testStylist2));
+  }
+  @Test
+  public void find_retrievesStylistWithSameId_testStylist2() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    testStylist1.save();
+    Stylist testStylist2 = new Stylist("Gladis", 8);
+    testStylist2.save();
+    assertEquals(Stylist.find(testStylist2.getId()), testStylist2);
+  }
+  @Test
+  public void updateWage_updateWageForStylist_12() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    testStylist1.save();
+    testStylist1.updateWage(12);
+    assertEquals(12, Stylist.find(testStylist1.getId()).getWage());
+  }
+  @Test
+  public void updateName_updateNameForStylist_Brandon() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    testStylist1.save();
+    testStylist1.updateName("Brandon");
+    assertEquals("Brandon", Stylist.find(testStylist1.getId()).getName());
+  }
+  @Test
+  public void delete_deletesAStylist_true() {
+    Stylist testStylist1 = new Stylist("Brenda", 10);
+    testStylist1.save();
+    int testStylistId = testStylist1.getId();
+    testStylist1.delete();
+    assertEquals(null, Stylist.find(testStylistId));
   }
 
 
