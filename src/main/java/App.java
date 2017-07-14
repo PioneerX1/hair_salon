5bf7762df489234b42f2d91dfc93566c1c45a398
Mick Sexton <mick@>
2017-07-14 14:34:42 -0700

    add new stylist form, confirm it adds the new stylist to the list of stylist and directs to his or her specific stylist page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,18 +25,23 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // get("stylist/new", (request, response) -> {
-    //
-    // })
-    //
-    // post("/stylist/:id", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   String name = request.queryParams("stylist-name");
-    //   int wage = Integer.parseInt(request.queryParams("stylist-wage"));
-    //   Stylist newStylist = new Stylist(name, wage);
-    //   newStylist.save();
-    //
-    // })
+    get("stylist-new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/add-stylist-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylist/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("stylist-name");
+      int wage = Integer.parseInt(request.queryParams("stylist-wage"));
+      Stylist stylist = new Stylist(name, wage);
+      stylist.save();
+      model.put("stylist", stylist);
+      model.put("clients", Client.findStylistId(stylist.getId()));
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
